622465471867adbdc08c90cff64426c2b41a93ea
Kappalucky <Shaquille.j.foster@gmail.com>
2021-03-22 13:30:08 -0400

    took a little while but finally have eslint and prettier working with vue

--- Timesheet-Application-Frontend/timesheet/src/store/index.js
+++ Timesheet-Application-Frontend/timesheet/src/store/index.js
@@ -1,11 +1,50 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import api from '../services/API';
+
+console.log(api)
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    timesheets: [],
+  },
+  mutations: {
+    ADD_TIMESHEETS(state, timesheet) {
+      state.timesheets = { ...timesheet };
+    },
+    NEW_VEHICLE(state, timesheet) {
+      state.timesheets.push(timesheet)
+    },
+  },
+  actions: {
+    newTimesheet({ dispatch, commit }, params) {
+      return new Promise((resolve, reject) => {
+        console.log(params);
+        api.post('/timesheets/', {
+          firstName: params.firstName,
+          lastName: params.lastName,
+          client: params.client,
+          project: params.project,
+          projectCode: params.projectCode,
+          hours: params.hours,
+          billable: params.billable,
+          billableRate: params.billableRate,
+          date: params.date
+        })
+          .then(response => {
+            console.log('newTimesheet:', response);
+            dispatch('getTimesheets');
+            resolve(response);
+          })
+          .catch(error => {
+            console.log(error);
+            reject(error);
+          })
+      })
+    },
+  },
   modules: {},
 });
